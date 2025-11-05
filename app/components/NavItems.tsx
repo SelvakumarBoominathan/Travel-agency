c83c61d62c3ceb0333f46928f7ae8909437a2949
Selvakumar Boominathan <selvans2k@gmail.com>
2025-11-05 10:19:13 +0530

    NavItems component

---
+++ app/components/NavItems.tsx
@@ -0,0 +1,13 @@
+import { Link } from "react-router";
+
+const NavItems = () => {
+  return (
+    <section className="nav-items">
+      {/* <Link to="/" className="link-logo" /> */}
+      <img src="/assets/icons/logo.svg" alt="logo" className="size-[30px]" />
+      <h1>Tourvisto</h1>
+    </section>
+  );
+};
+
+export default NavItems;
--- app/routes.ts
+++ app/routes.ts
@@ -3,6 +3,6 @@ import { type RouteConfig, route, layout } from "@react-router/dev/routes";
 export default [
   layout("routes/admin/admin_layout.tsx", [
     route("dashboard", "routes/admin/dashboard.tsx"),
-    route("AllUsers", "routes/admin/all_users.tsx"),
+    route("all-users", "routes/admin/all_users.tsx"),
   ]),
 ] satisfies RouteConfig;
