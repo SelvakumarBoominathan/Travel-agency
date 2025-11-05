e50cb0f1ea734540ef24c79fa08d19ef893a25b6
Selvakumar Boominathan <selvans2k@gmail.com>
2025-11-05 09:33:40 +0530

    Initial commit

---
+++ app/routes.ts
@@ -0,0 +1,7 @@
+import { type RouteConfig, route, layout } from "@react-router/dev/routes";
+
+export default [
+  layout("routes/admin/admin_layout.tsx", [
+    route("dashboard", "routes/admin/dashboard.tsx"),
+  ]),
+] satisfies RouteConfig;
